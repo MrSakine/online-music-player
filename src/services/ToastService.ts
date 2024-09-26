f1989162e90c7cc080fc0b43cdcce2279c4d08bd
MrSakine <msakine20@gmail.com>
2024-09-26 19:56:19 +0000

    vercel config

--- src/services/ToastService.ts
+++ src/services/ToastService.ts
@@ -1,7 +1,7 @@
 import { Bounce, toast } from "react-toastify";
 
 const TOAST_OPTIONS = {
-  autoClose: 5000,
+  autoClose: 3000,
   hideProgressBar: false,
   closeOnClick: true,
   // pauseOnHover: true,
